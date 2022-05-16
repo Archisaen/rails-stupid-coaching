4beeedc76ae5c2c2c4246de365bbfee85ed672a7
Archibald Saen <archibaldsaen98@gmail.com>
2022-05-16 14:42:15 +0200

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def answer
+    @query = params[:question]
+    @answer = coach_answer(@query) if @query
+  end
+
+  def ask
+  end
+
+  def coach_answer(your_message)
+    if your_message == 'I am going to work'
+      'Great!'
+    elsif your_message.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
